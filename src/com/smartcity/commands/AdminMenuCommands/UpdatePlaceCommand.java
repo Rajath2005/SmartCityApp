f860cd4091c5fb589e8a3221585030b595d73674
Robert Palmer <Awriter247@gmail.com>
2026-08-24 16:13:14 -0400

    Refactor: Implement Command Design Pattern for managing city resources and add command classes for adding, updating, and deleting places

---
+++ src/com/smartcity/commands/AdminMenuCommands/UpdatePlaceCommand.java
@@ -0,0 +1,12 @@
+package com.smartcity.commands.AdminMenuCommands;
+
+import com.smartcity.commands.Command;
+import com.smartcity.main.SmartCityApp;
+
+public class UpdatePlaceCommand implements Command {
+
+    @Override
+    public void execute() {
+        // SmartCityApp.updatePlace();
+    }
+}
